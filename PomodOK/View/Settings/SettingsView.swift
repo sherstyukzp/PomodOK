03c4e12a5b2421b6f8ae04ef7ee82d0084380b58
Ярослав Шерстюк <sherstyuk.ya.n@gmail.com>
2022-11-02 16:03:51 +0200

    - add delete Item Core Data

--- PomodOK/View/Settings/SettingsView.swift
+++ PomodOK/View/Settings/SettingsView.swift
@@ -9,11 +9,15 @@
 // https://www.simpleswiftguide.com/how-to-use-userdefaults-in-swiftui/
 
 import SwiftUI
+import CoreData
 import StoreKit
 
 struct SettingsView: View {
-    
+
     @Environment(\.presentationMode) var presentationMode
+    // MARK: - Core Data
+    @Environment(\.managedObjectContext) var moc
+    
     @ObservedObject var userSettings = UserSettings()
     
     @AppStorage("isNotificationsEnabled") var isNotificationsEnabled: Bool = true
@@ -23,6 +27,13 @@ struct SettingsView: View {
     @AppStorage("workSession") var workSession: Int = 25
     @AppStorage("shortBreak") var shortBreak: Int = 5
     @AppStorage("longBreak") var longBreak: Int = 15
+    
+    @FetchRequest(sortDescriptors: [
+        SortDescriptor(\Item.timestamp, order: .reverse)])
+    var fetchedItems: FetchedResults<Item>
+    // MARK: - @State
+    @State private var showingDeleteAlert = false
+    @State private var isExportCSVSheetShowing = false
     
     var notificationPublisher = NotificationManager()
     
@@ -37,11 +48,9 @@ struct SettingsView: View {
     
     // MARK: - Body
     var body: some View {
-        
         NavigationView {
             Form {
                 Section(header: HeaderSettingView(imageIcon: "bell", text: "NOTIFICATIONS")) {
-                    
                     Toggle(isOn: $isNotificationsEnabled) {
                         Text("Push notifications")
                     }
@@ -63,6 +72,23 @@ struct SettingsView: View {
                     }
                     Stepper(value: $longBreak, in: 1...60) {
                         Text("Long Break \(longBreak) min")
+                    }
+                }
+                
+                if !fetchedItems.isEmpty {
+                    Section(header: HeaderSettingView(imageIcon: "tray.2", text: "Data")) {
+                        // Экспорт
+                        Button {
+                            exportOverlayCSV()
+                        } label: {
+                            Label("Export in CSV", systemImage: "square.and.arrow.up")
+                        }
+                        // ивдалення всіх даних
+                        Button {
+                            self.showingDeleteAlert = true
+                        } label: {
+                            Label("Delete All", systemImage: "trash")
+                        }
                     }
                 }
                 
@@ -106,7 +132,64 @@ struct SettingsView: View {
                     .foregroundColor(Color(.red))
             }
             )
+            .alert(isPresented: $showingDeleteAlert) {
+                Alert(title: Text("WARNING!"),
+                      message: Text("Do you really want to delete all log data?"),
+                      primaryButton: .destructive(Text("Yes")) {
+                    self.deleteAllRecords()},
+                      secondaryButton: .cancel()
+                )
+            }
         }
+    }
+    
+    // MARK: - Удаление всех записей журнала
+    func deleteAllRecords() {
+        
+        let deleteFetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Item")
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: deleteFetch)
+        
+        do {
+            try moc.execute(deleteRequest)
+            try moc.save()
+            moc.reset()
+        } catch {
+            print ("There was an error")
+        }
+        
+    }
+    // MARK: - Export object CSV
+    func exportOverlayCSV() {
+        let fileName = "PomodOK.csv"
+        let path = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(fileName)
+        var csvText = "Date\n"
+        
+        for object in fetchedItems {
+            csvText += "\(object.timestamp?.formatted() ?? "Not Item")\n"
+        }
+        
+        do {
+            try csvText.write(to: path!, atomically: true, encoding: String.Encoding.utf8)
+        } catch {
+            print("Failed to create file")
+            print("\(error)")
+        }
+        print(path ?? "not found")
+        
+        var filesToShare = [Any]()
+        filesToShare.append(path!)
+        
+        let av = UIActivityViewController(activityItems: filesToShare, applicationActivities: nil)
+        
+        if let vc = UIApplication.shared.windows.first?.rootViewController{
+            av.popoverPresentationController?.sourceView = vc.view
+            //Setup share activity position on screen on bottom center
+            av.popoverPresentationController?.sourceRect = CGRect(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height, width: 0, height: 0)
+            av.popoverPresentationController?.permittedArrowDirections = UIPopoverArrowDirection.down
+            vc.present(av, animated: true, completion: nil)
+        }
+        
+        isExportCSVSheetShowing.toggle()
     }
     
     // MARK: - Toggle Notification
